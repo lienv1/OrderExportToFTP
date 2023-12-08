c1a7d60814704f465ea9d8761fa9a8ce43b1f1bb
lienv1 <bobby_lien@hotmail.com>
2023-12-08 15:43:40 +0100

    update

---
+++ src/main/java/OrderExportToFTP/OrderExportToFTP/FTPController.java
@@ -0,0 +1,17 @@
+package OrderExportToFTP.OrderExportToFTP;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ftpservice")
+public class FTPController {
+
+	public FTPController() {
+		
+	}
+	
+	
+	
+	
+}
