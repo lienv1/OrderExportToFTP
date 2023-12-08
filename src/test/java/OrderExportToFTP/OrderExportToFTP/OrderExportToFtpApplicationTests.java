7df57a55b68651ca8fc52cc065771b8fb70965d2
lienv1 <bobby_lien@hotmail.com>
2023-12-08 15:29:49 +0100

    initial commit

---
+++ src/test/java/OrderExportToFTP/OrderExportToFTP/OrderExportToFtpApplicationTests.java
@@ -0,0 +1,13 @@
+package OrderExportToFTP.OrderExportToFTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderExportToFtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
